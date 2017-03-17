9906258c1777411aef90f54f2c342e025e9379c2
Philip Heinser <philipheinser@me.com>
2017-03-17 14:57:46 +0100

    windows stub

---
+++ index.windows.js
@@ -0,0 +1,17 @@
+/**
+ * Stub of BeaconBroadcast for Windows.
+ *
+ * @providesModule BeaconBroadcast
+ * @flow
+ */
+'use strict';
+
+var warning = require('fbjs/lib/warning');
+
+var BeaconBroadcast = {
+  test: function() {
+    warning('Not yet implemented for Windows.');
+  }
+};
+
+module.exports = BeaconBroadcast;
